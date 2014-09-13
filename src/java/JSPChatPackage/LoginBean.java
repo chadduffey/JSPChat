3ef2372a5d713cdf524ddb6e0c12cb86091d944b
Chad Duffey <chadduffey@hotmail.com>
2014-09-13 13:10:47 -0700

    Added Basic Authentication with DB

--- src/java/JSPChatPackage/LoginBean.java
+++ src/java/JSPChatPackage/LoginBean.java
@@ -1,5 +1,11 @@
 package JSPChatPackage;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class LoginBean {
     private String username;
     private String password;
@@ -20,13 +26,33 @@ public class LoginBean {
         this.password = password;
     }
     
-    public boolean checkLogin(){
+    public boolean checkLogin(String uname, String pwd){
         
-        if("admin".equals(getUsername()) && "admin".equals(getPassword())){
-            return true;
+        //open a DB connection
+        try {
+            String dbURL = "jdbc:mysql://localhost:3306/jspchat";
+            String dbUsername = "root";
+            String dbPassword = "Password123";
+            Connection connection = DriverManager.getConnection(dbURL, dbUsername, dbPassword);
+
+            Statement statement = connection.createStatement();
+            ResultSet users = statement.executeQuery("SELECT * FROM user");
+
+            //check if the username exists
+            while (users.next()){
+                if (users.getString("username").equals(uname)){
+                    //we found the user in the db
+                    if (users.getString("password").equals(pwd)){
+                        //the password matches as well
+                        return true;
+                    }
+                }
+            }
+        } catch (SQLException e) {
+                e.printStackTrace();
         }
         
+        //we didnt find the username and password combo.
         return false;
     }
-    
 }
--- src/java/JSPChatPackage/loginController.java
+++ src/java/JSPChatPackage/loginController.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package JSPChatPackage;
 
 import java.io.IOException;
@@ -12,22 +8,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author chadd_000
- */
 public class loginController extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP
-     * <code>GET</code> and
-     * <code>POST</code> methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
@@ -48,36 +30,16 @@ public class loginController extends HttpServlet {
         }
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP
-     * <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
-     * Handles the HTTP
-     * <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
-        //response.setContentType("text/html");
         
         String username = request.getParameter("username");
         String password = request.getParameter("password");
@@ -86,7 +48,7 @@ public class loginController extends HttpServlet {
         loginBean.setUsername(username);
         loginBean.setPassword(password);
         
-        if(loginBean.checkLogin()){
+        if(loginBean.checkLogin(loginBean.getUsername(), loginBean.getPassword())){
             //login success
             RequestDispatcher dispatch = request.getRequestDispatcher("/loginSuccess.jsp");
             dispatch.forward(request, response);
@@ -100,11 +62,6 @@ public class loginController extends HttpServlet {
         processRequest(request, response);
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
     @Override
     public String getServletInfo() {
         return "Short description";
